440bf08417001d21703bdb53621fa533c2e0011b
illegal-instruction-co <berkay@illegal-instruction.com>
2023-02-09 01:37:50 +0300

    improve tool

--- tools/bin2mac.py
+++ tools/bin2mac.py
@@ -1,20 +1,28 @@
 from macaddress import MAC
 import sys
 
-if len(sys.argv) < 2:
-    print("Usage: %s <shellcode_file>" % sys.argv[0])
-    sys.exit(1) 
+outfile = sys.argv[1] + '.c'
+temp = sys.stdout
 
-with open(sys.argv[1], "rb") as f:
-    chunk = f.read(6)
-    print("{}const char* MAC[] =".format(' '*4))
-    print("    {")
-    while chunk:
-        if len(chunk) < 6:
-            padding = 6 - len(chunk)
-            chunk = chunk + (b"\x90" * padding)
-            print("{}\"{}\"".format(' '*8,MAC(chunk)))
-            break
-        print("{}\"{}\",".format(' '*8,MAC(chunk)))
+with open(outfile, 'w') as sys.stdout:
+
+    if len(sys.argv) < 2:
+        print("Usage: %s <shellcode_file>" % sys.argv[0])
+        sys.exit(1) 
+
+    with open(sys.argv[1], "rb") as f:
         chunk = f.read(6)
-    print("    };")
+        print("{}const char* MAC[] =".format(' '*4))
+        print("    {")
+        while chunk:
+            if len(chunk) < 6:
+                padding = 6 - len(chunk)
+                chunk = chunk + (b"\x90" * padding)
+                print("{}\"{}\"".format(' '*8,MAC(chunk)))
+                break
+            print("{}\"{}\",".format(' '*8,MAC(chunk)))
+            chunk = f.read(6)
+        print("    };")
+
+sys.stdout = temp
+print('Successfully wroten as MAC to ' + outfile)
